6d7fce7091f48b43281f42f7bff158c580237826
Sem8 <mssemmi8@gmail.com>
2019-01-29 18:07:21 -0500

    work in progress MVP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+
+// Thought type writer on double click
+
+let thoughtBtn = document.getElementById('thought-button');
+thoughtBtn.style.background = '#00FFFF';
+thoughtBtn.style.height = '40px';
+thoughtBtn.style.borderRadius = '50px';
+thoughtBtn.style.fontSize = '2rem';
+
+let i = 0;
+let thoughtText = 'I badly need a vacation... from EVERYTHING';
+
+let speed = 50;
+
+function thoughtTypeWriter() {
+    if(i < thoughtText.length) {
+        document.getElementById('thought-text').innerHTML += thoughtText.charAt(i);
+        i++;
+        setTimeout(thoughtTypeWriter, speed)
+    }
+}
+thoughtBtn.addEventListener('dblclick', thoughtTypeWriter);
+
+// // Enlarge text on mouseover
+
+// let textContent = document.querySelectorAll('.text-content p');
+// textContent.addEventListener('mouseover', )
+
+// swap text
+let contentSection = document.querySelector('.content-section');
+function changeBackground(e) {    
+    contentSection.style.background = "rgb("+e.offsetX+","+e.offsetY+",40)";
+}
+
+function resetBackground() {
+    contentSection.style.background = "white";
+
+}
+
+
+let swapTextBtn = document.getElementById('swap-text');
+swapTextBtn.style.background = '#00FFFF';
+swapTextBtn.style.borderRadius = '50px';
+swapTextBtn.style.fontSize = '2rem';
+
+swapTextBtn.addEventListener('mouseover', changeBackground);
+swapTextBtn.addEventListener('mouseout', resetBackground);
+
+
+
+    
