7676078b6ce7500c6e3adf62ce0e550422984abb
Sem8 <mssemmi8@gmail.com>
2019-01-29 21:44:24 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -63,6 +63,94 @@ disappearBtn.style.fontSize = '2rem';
 disappearBtn.addEventListener('mousedown', disappearDiv);
 
 // appear div
+let selectBtn = document.getElementById('select-text');
+function selectAlert() {
+    document.getElementById('appear-text').innerHTML = 'And well make all your dreams come true!';
+    }
+
+selectBtn.addEventListener('select', selectAlert);
+
+function focusFunc() {
+    selectBtn.style.background = '#00FFFF';    
+}
+
+selectBtn.addEventListener('focus', focusFunc);
+
+
+
+let enlargeText = document.querySelectorAll('.destination');
+let enlargedParagraphs = document.querySelectorAll('.destination p');
+console.log(enlargedParagraphs);
+
+function fontChangeFunc() {
+    enlargedParagraphs[0].style.fontSize = '3rem'; 
+    enlargedParagraphs[1].style.fontSize = '3rem';  
+    enlargedParagraphs[2].style.fontSize = '3rem';    
+}
+function colorChangeFunc() {
+    enlargedParagraphs[0].style.color = 'red'; 
+    enlargedParagraphs[1].style.color = 'red';  
+    enlargedParagraphs[2].style.color = 'red'; 
+
+}
+
+enlargeText.forEach((item) => {
+    item.addEventListener('wheel', fontChangeFunc);
+});
+enlargeText.forEach((item) => {
+    item.addEventListener('mouseup', colorChangeFunc);
+});
+
+
+// let home = document.querySelector('.nav-link');
+// console.log(home);
+// home.addEventListener('load', alertFunc);
+// function alertFunc() {
+//  let example = document.querySelector('.nav-link');
+//  console.log(example);  
+//  example.style.backgroundColor = 'red';
+ 
+
+// }
+// // alertFunc();
+
+let funBusImg = document.querySelector('.intro img');
+console.log(funBusImg);
+
+function bodyColorChange(event) {
+    document.body.style.backgroundColor = "rgb("+event.offsetX+","+event.offsetY+",40)";
+}
+function bodyColorNormal() {
+    document.body.style.backgroundColor = 'white';
+}
+
+funBusImg.addEventListener('mouseenter', bodyColorChange);
+
+funBusImg.addEventListener('mouseleave', bodyColorNormal);
+
+
+let containerClick = document.querySelector('.home');
+containerClick.addEventListener('click', function anEvent(e) {
+    console.log(`I just clicked container`);
+});
+
+let introClick = document.querySelector('.intro');
+introClick.addEventListener('click', function anotherEvent(e) {
+    console.log(`I only clicked on intro`);
+    e.stopPropagation();
+});
+
+let preventRefresh = document.querySelectorAll('.nav-link');
+// preventRefresh[0].addEventListener('click', function noRefresh(e) {
+//     e.preventDefault();
+// });
+
+preventRefresh.forEach((eachItem) => {
+    eachItem.addEventListener('click', function noRefresh(e) {
+    e.preventDefault();
+})
+});
+    
 
 
 
